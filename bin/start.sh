c9d2c25114e6c34d74250b99371bc305529f8f52
taoistwar <taoistwar@gmail.com>
2026-04-15 15:12:43 +0800

    start.sh add cd

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+echo $BASE
+cd $BASE
 mkdir -p $BASE/logs
 nohup $BASE/bin/ollama-audit > $BASE/logs/ollama-audit.log 2>&1 &
